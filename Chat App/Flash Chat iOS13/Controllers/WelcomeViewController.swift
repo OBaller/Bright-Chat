b9e5dfd8d25b316be5959765cac57c0eae3a9ea0
Naseem Oyebola <naseemoyebola@gmail.com>
2021-06-13 13:34:26 +0100

    set up and segues

---
+++ Chat App/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = K.appName
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titletext = "⚡️FlashChat"
+//        for letter in titletext {
+//            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+        
+       
+    }
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+}
+}
